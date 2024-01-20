c16bf8eed3530c8493f7c6ca8190fd3a8734c95f
Xritre <98012222+Xritre@users.noreply.github.com>
2024-01-20 16:38:45 +0700

    Delete dash and merge animation script
    - Delete dash script and scene that won't be used because dash need to be inside movement handler script
    - Rename flip_animation into character_animation
    - Delete play_animation and move playAnimation function into character_animation
    - Update player script by removing dash and change path of flip_animation to character_animation

--- player.gd
+++ player.gd
@@ -1,17 +1,12 @@
 extends CharacterBody3D
 
-const normalSpeed : int  = 3
-const dashSpeed : int = 6
 var lastDirection : int = EntityDirection.right
-var direction = Vector3.ZERO
-var dashDuration = 0.15
+var direction : Vector3 = Vector3.ZERO
 @onready var animationSprite = get_node("AnimatedSprite3D")
 @onready var animationPlayer = get_node("AnimationPlayer")
-@onready var flipAnimation = preload("res://flip_animation.gd").new()
+@onready var characterAnimation = preload("res://character_animation.gd").new()
 @onready var movement = preload("res://movement_handler.gd").new()
 @onready var playerInput = preload("res://player_input.gd").new()
-@onready var playAnimation = preload("res://play_animation.gd").new()
-@onready var dash = $Dash
 func _physics_process(delta):
 	playerMovement()
 	playerAnimation(delta)
@@ -20,21 +15,19 @@ func _physics_process(delta):
 
 func playerMovement():
 	#Check user input movement
-	playerInput.checkPlayerInput(dash,dashDuration)
+	playerInput.checkPlayerInput()
 	#Assign directional input 
 	direction = playerInput.direction
 	#Update last direction of player facing
-	lastDirection = flipAnimation.updateLastDirectionHorizontal(direction.x,lastDirection)
-	#If player is dashing set speed for dash, else set to normal/walk speed
-	var speed : int = dashSpeed if dash.isDashing() else normalSpeed
+	lastDirection = characterAnimation.updateLastDirection(direction.x,lastDirection)
 	#Calculate player movement in 4 directional
-	velocity = movement.movementHandler(direction,speed)
+	velocity = movement.movementHandler(direction,ConstantNumber.playerSpeed, playerInput.dash)
 	#Reset the directional input of playerInput function to zero vector
 	playerInput.resetDirection()
 
 func playerAnimation(delta : float):
 	#Play animation of player by the movement of player
-	playAnimation.selectAnimation(animationPlayer,velocity)
+	characterAnimation.playAnimation(animationPlayer,velocity)
 	#Flip direction of player 
-	flipAnimation.flipAnimation(lastDirection,animationSprite, delta * ConstantNumber.flipConstant)
+	characterAnimation.flipAnimation(lastDirection,animationSprite, delta * ConstantNumber.flipConstant)
 

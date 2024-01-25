7a0c8699821b2ab3a554f2bd1c26543d34749441
ArtMuchimuchi <artaoom21@gmail.com>
2024-01-25 13:40:46 +0700

    player dash
    make dash function and reconstruct code in physic_process

--- player.gd
+++ player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 var lastDirection : int = EntityDirection.right
 var direction : Vector3 = Vector3.ZERO
 var isDash : bool = false
+var dashCountdown : float = 0
 
 @onready var animationSprite = get_node("AnimatedSprite3D")
 @onready var animationPlayer = get_node("AnimationPlayer")
@@ -10,17 +11,24 @@ var isDash : bool = false
 @onready var movement = preload("res://movement_handler.gd").new()
 
 func _physics_process(delta):
-	playerMovement()
-	playerAnimation(delta)
+	#Check user input movement
+	direction = movement.checkPlayerInput()
+	#Update last direction of player facing
+	lastDirection = movement.updateLastDirection(direction.x,lastDirection)
+	#Check is player dash or not
+	if Input.is_action_just_pressed("dash"):
+		isDash = true
+	#if player dash then do the dash thing, other wise do movement
+	if(isDash):
+		dash(delta)
+	else :
+		playerMovement()
+		playerAnimation(delta)
 	
 	move_and_slide()
 
 
 func playerMovement():
-	#Check user input movement
-	checkPlayerInput()
-	#Update last direction of player facing
-	lastDirection = animationManager.updateLastDirection(direction.x,lastDirection)
 	#Calculate player movement in 4 directional
 	velocity = movement.movementHandler(direction, ConstantNumber.playerSpeed)
 	#Reset the directional input of playerInput function to zero vector
@@ -31,24 +39,23 @@ func playerAnimation(delta : float):
 	animationManager.movementAnimation(animationPlayer,velocity)
 	#Flip direction of player 
 	animationManager.flipAnimation(lastDirection,animationSprite, delta)
-
-func checkPlayerInput():
-	# Check 4 direction movement that player could control
-	if Input.is_action_pressed("move_forward"):
-		direction.z -= ConstantNumber.directionConstant
-	if Input.is_action_pressed("move_back"):
-		direction.z += ConstantNumber.directionConstant
-	if Input.is_action_pressed("move_left"):
-		direction.x -= ConstantNumber.directionConstant
-	if Input.is_action_pressed("move_right"):
-		direction.x += ConstantNumber.directionConstant
 	
-	if Input.is_action_just_pressed("dash"):
-		isDash = true
-		
-	# Make Diagonal movement same speed as horizontal and vertical
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		
 func resetDirection():
 	direction = Vector3.ZERO
+
+func dash(delta: float):
+	if isDash == true:
+		# Move when there is direction
+		if direction: 
+			velocity.x = direction.x * 7 
+			velocity.z = direction.z * 7
+		else:
+			if(lastDirection == EntityDirection.right):
+				velocity.x = 1 * 7
+			elif (lastDirection == EntityDirection.left):
+				velocity.x = -1 * 7
+		if (dashCountdown >= 0.08) :
+			isDash = false
+			dashCountdown = 0
+		else :
+			dashCountdown += delta

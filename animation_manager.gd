98d14111e5b2e904c4d6da4ac885753ecf782a25
ArtMuchimuchi <artaoom21@gmail.com>
2024-01-25 13:38:30 +0700

    reorganize function
    -move updateLastDirection from animationManager to movementHandler
    -move checkInputPlayer from player.gd to movementHandler

--- animation_manager.gd
+++ animation_manager.gd
@@ -7,13 +7,6 @@ func movementAnimation(animationPlayer: AnimationPlayer,velocity : Vector3):
 	#If character does move, play run animation
 	elif velocity.x != ConstantNumber.noMovementConstant || velocity.z != ConstantNumber.noMovementConstant:
 		animationPlayer.play("Run")
-
-func updateLastDirection(velocity:float, lastDirection : int): 
-	if velocity > ConstantNumber.noMovementConstant: 
-		lastDirection = EntityDirection.right
-	elif  velocity < ConstantNumber.noMovementConstant:
-		lastDirection = EntityDirection.left
-	return lastDirection
 	
 func flipAnimation(lastDirection : int, animatedSprite : AnimatedSprite3D, delta : float):
 	var rotation : int

fd82993eae6776f2aac9c51f1dd740ebd577e37a
Xritre <98012222+Xritre@users.noreply.github.com>
2024-02-13 18:46:42 +0700

    Edit main menu
    - Add new buttons such as upgrade and setting
    - Add label for game title
    - Attach pressed signal for both upgrade and setting buttons

--- main_menu.gd
+++ main_menu.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Control
 
 
 func _on_quit_button_pressed():
@@ -7,3 +7,14 @@ func _on_quit_button_pressed():
 
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://mock_scene.tscn")
+
+
+func _on_upgrade_button_pressed():
+	pass
+
+
+func _on_options_button_pressed():
+	pass
+
+
+

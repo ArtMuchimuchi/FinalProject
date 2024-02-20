dae717f8c7c52203bab8eb7a9f254d14fc25c7a3
ArtMuchimuchi <artaoom21@gmail.com>
2024-02-20 15:39:03 +0700

    enemy_attack
    - add enemy behavior in enemy.gd
    - add function damaged in player.gd
    - add new State tried in EntityState
    - add triedCountdown variable in entity.gd
    - add enemyTriedDuration in constant_number.gd
    - add tried handle in enemyMovement at movement_handler.gd
    - fixed bug in die function

--- utils/constant/entity_state.gd
+++ utils/constant/entity_state.gd
@@ -1,7 +1,8 @@
 class_name EntityState
 
-const knockBack : int = 3
-const dash : int = 3
+const knockBack : int = 4
+const dash : int = 4
+const tried : int = 3
 const attacking : int = 2
 const moving : int = 1
 const idle : int = 0

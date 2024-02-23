8dd6ae5afd491245dfe8b8b00e06930bd5593864
ArtMuchimuchi <artaoom21@gmail.com>
2024-02-23 14:54:38 +0700

    new_attack
    - change attack function to meleeAttack function
    - change knockback function so it can take knockback parameter
    - change enemy knockback parameters due to aboce changes
    - add dashDuration variable for knockback calculation
    - adjust player attack function
    - adjust enemy knockback behavior

--- utils/constant/constant_number.gd
+++ utils/constant/constant_number.gd
@@ -9,6 +9,7 @@ const playerDashSpeed : int = 17
 const playerDashDuration : float = 0.08
 const playerHealthPoint : int = 10
 const playerMeleeDamage : int = 3
+const playerRangeDamage : int = 1
 
 const enemyHealthPoint : int = 10
 const enemySpeed : int = 4
@@ -16,3 +17,7 @@ const enemyDashSpeed : int = 14
 const enemyDashDuration : float = 0.3
 const enemyMeleeDamage : int = 1
 const enemyTriedDuration : float = 0.5
+const enemyMeleeKnockbackSpeed : int = 14
+const enemyMeleeKnockbackDuration : float = 0.3
+const enemyRangeKnockbackSpeed : int = 7
+const enemyRangeKnockbackDuration : float = 0.1

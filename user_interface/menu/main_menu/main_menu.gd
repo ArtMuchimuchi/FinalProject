53498b87f1477321726dc5a19594975cca874d76
Xritre <98012222+Xritre@users.noreply.github.com>
2024-02-24 20:42:02 +0700

    Add upgrade slot to main menu
    - Add upgrade slot scene inside main menu scene
    - Add ready func to Connect signal of back button in option menu and closed button in upgrade menu
    - Add function to closed upgrade menu

--- user_interface/menu/main_menu/main_menu.gd
+++ user_interface/menu/main_menu/main_menu.gd
@@ -2,7 +2,15 @@ extends Control
 
 @onready var main_menu_margin_container = $MarginContainer
 @onready var options_menu = $OptionsMenu
+@onready var upgrade_menu = $UpgradeMenu
 
+
+func _ready():
+	# Connect signal of pressed back button in option menu
+	options_menu.back_options_menu.connect(_on_back_options_menu)
+	# Connect signal of pressed closed button in upgrade menu	
+	upgrade_menu.closed_upgrade_menu.connect(_on_closed_upgrade_menu)
+	
 
 func _on_quit_button_pressed():
 	get_tree().quit()
@@ -13,7 +21,8 @@ func _on_start_button_pressed():
 
 
 func _on_upgrade_button_pressed():
-	pass
+	main_menu_margin_container.visible = false
+	upgrade_menu.visible = true
 
 
 func _on_options_button_pressed():
@@ -21,6 +30,10 @@ func _on_options_button_pressed():
 	options_menu.visible = true
 
 
-func _on_options_menu_back_options_menu():
+func _on_back_options_menu():
 	main_menu_margin_container.visible = true
 	options_menu.visible = false
+
+func _on_closed_upgrade_menu():
+	main_menu_margin_container.visible = true
+	upgrade_menu.visible = false

e978fc03fcabc85f7bc5419e60a0091afa8d385e
Xritre <98012222+Xritre@users.noreply.github.com>
2024-02-26 15:33:03 +0700

    Rename and delete slot data model
    - Delete upgrade_slot.gd in slot_data_model folder because it's unnecessary to have file with classname that has only single array
    - Rename slot_data_model folder -> trait_slot_data_model
    - Rename slot_data.gd file -> trait_slot_data.gd  and classname Slot -> TraitSlotData
    - Rename slot_level_data.gd file -> trait_slot_level_data and classname SlotLevel -> TraitSlotLevelData

--- user_interface/menu/upgrade_menu/trait_slot_data_model/trait_slot_data.gd
+++ user_interface/menu/upgrade_menu/trait_slot_data_model/trait_slot_data.gd
@@ -1,11 +1,11 @@
 extends Resource
 
-class_name Slot
+class_name TraitSlotData
 
 @export var name : String = ""
 @export var texture : Texture
 @export var current_level : int = 0
-@export var slot_level : Array[SlotLevel]
+@export var slot_level : Array[TraitSlotLevelData]
 
 func upgrade_level():
 	current_level += 1
--- user_interface/menu/upgrade_menu/trait_slot_data_model/trait_slot_level_data.gd
+++ user_interface/menu/upgrade_menu/trait_slot_data_model/trait_slot_level_data.gd
@@ -1,6 +1,6 @@
 extends Resource
 
-class_name SlotLevel
+class_name TraitSlotLevelData
 
 @export var description : String 
 @export var price : int 

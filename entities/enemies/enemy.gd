8eaab58fc8fc8e48d7106f7be14c0c8e2e5b7351
ArtMuchimuchi <artaoom21@gmail.com>
2024-02-26 16:07:26 +0700

    projectile attack
    - create new scene projectile attack
    
    ---- enemy.gd ----
    - rename meleeAttack -> meleeAttackManager
    - create rangeAttackManager
    - create meleeAttack function
    - create rangeAttack function
    
    ---- player.gd ----
    - fixed player damaged function

--- entities/enemies/enemy.gd
+++ entities/enemies/enemy.gd
@@ -6,15 +6,18 @@ class_name Enemy
 @onready var animationSprite = get_node("AnimatedSprite3D")
 @onready var animationPlayer = get_node("AnimationPlayer")
 @onready var hitboxMeleeAttack = get_node("HitBoxMeleeAttack")
+@onready var hitboxRangeAttack = get_node("HitBoxRangeAttack")
 
 @onready var movement = MovementHandler.new(self)
 @onready var animationManager = AnimationManager.new()
 var HP : HealthPoint
-@onready var meleeAttack = AttackHandler.new(self, hitboxMeleeAttack)
+@onready var meleeAttackManager = AttackHandler.new(self, hitboxMeleeAttack)
+@onready var rangeAttackManager = AttackHandler.new(self, hitboxRangeAttack)
 
 func _init():
 	initEntity()
 	meleeAttackDamage = ConstantNumber.enemyMeleeDamage
+	rangeAttackDamage = 1
 	healthPoint = ConstantNumber.enemyHealthPoint
 	movementSpeed = ConstantNumber.enemySpeed
 	dashSpeed = 0
@@ -43,18 +46,34 @@ func animation(delta: float):
 	animationManager.flipAnimation(lastDirection, animationSprite, delta)
 	
 func attack():
+	meleeAttack()
+		
+func meleeAttack():
 	#update melee attack hitbox
-	meleeAttack.updateHitbox()
+	meleeAttackManager.updateHitbox()
 	#create variable and check for player
 	var isNearPlayer = hitboxMeleeAttack.get_overlapping_bodies()
-	if(isNearPlayer.is_empty()):
-		pass
-	else:
+	if(!isNearPlayer.is_empty()):
 		#if player found, attacking
 		movement.setState(EntityState.attacking)
 		if(movementState == EntityState.attacking):
 			#deal damage
-			meleeAttack.meleeAttack(meleeAttackDamage)
+			meleeAttackManager.meleeAttack(meleeAttackDamage)
+			#tried after attack for delay
+			movement.setState(EntityState.tried)
+			
+func rangeAttack():
+	#create variable and check for player
+	var isNearPlayer = hitboxRangeAttack.get_overlapping_bodies()
+	if(!isNearPlayer.is_empty()):
+		#if player found, attacking
+		movement.setState(EntityState.attacking)
+		if(movementState == EntityState.attacking):
+			#create projectile and shoot in player current position
+			var projectile = preload("res://entities/projectile_attack.tscn").instantiate()
+			projectile.direction = (player.global_position - position).normalized()
+			projectile.damage = rangeAttackDamage
+			self.add_child(projectile)
 			#tried after attack for delay
 			movement.setState(EntityState.tried)
 	

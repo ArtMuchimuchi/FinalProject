03eafc3c16621b3add469740747491c68102412f
Xritre <98012222+Xritre@users.noreply.github.com>
2024-03-03 22:19:33 +0700

    Add buff data model and mockup data
    - Add buff_data_model, mockup_buff_data subfolder in buff folder
    - Add buff_data.gd model in buff_data_model folder for basic data that will show in hud interface such as name, texture (buff icon), current_level and buff_level(Which show each level description)
    - Add buff_level_data.gd model for show different of variable like description in each buff level
    - Add mockup buff data 1, 2, 3

---
+++ user_interface/hud/buff/buff_data_model/buff_data.gd
@@ -0,0 +1,8 @@
+extends Resource
+
+class_name BuffData
+
+@export var name : String
+@export var texture : Texture
+@export var currentLevel : int = 0
+@export var buffLevel : Array[BuffLevelData] 
---
+++ user_interface/hud/buff/buff_data_model/buff_level_data.gd
@@ -0,0 +1,5 @@
+extends Resource
+
+class_name BuffLevelData
+
+@export var description : String

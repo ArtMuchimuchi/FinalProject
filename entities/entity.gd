f3b21c0882c783a848c9a178b19d96b7bf67c72e
Xritre <98012222+Xritre@users.noreply.github.com>
2024-03-03 22:02:31 +0700

    Add signal for HP changed in Entity.gd
    - Add currentHP_changed in Entity.gd to send changedHP
    - Refactor increaseHP and decreaseHP function to emit currentHP_changed of Entity node

--- entities/entity.gd
+++ entities/entity.gd
@@ -13,6 +13,7 @@ var lastDirection : int
 var direction : Vector3 
 var movementCountdown : float
 var triedCountdown : float
+signal currentHP_changed(changedHP : int)
 
 #initiate varables for entity
 func initEntity():
--- entities/health_point.gd
+++ entities/health_point.gd
@@ -4,20 +4,22 @@ class_name HealthPoint
 
 var ownerNode : Entity
 var currentHP : int
-	
+
 func _init(targetNode: Entity, setHP: int):
 	ownerNode = targetNode
 	currentHP = setHP
 	
 func  increaseHP(increaseAmount: int):
 	currentHP += increaseAmount
-	
+	ownerNode.emit_signal("currentHP_changed",currentHP)
+
 func decreaseHP(decreaseAmount: int):
 	if(decreaseAmount >= currentHP):
 		die()
 	else:
 		currentHP -= decreaseAmount
-	
+		ownerNode.emit_signal("currentHP_changed",currentHP)
+		
 func die():
 	if(ownerNode.name == "Player"):
 		ownerNode.get_tree().paused = true

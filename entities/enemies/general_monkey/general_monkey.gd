ba296a4744896512403aa7e46e11d5b6cdbbd885
ArtMuchimuchi <artaoom21@gmail.com>
2024-03-15 19:57:02 +0700

    create_general_monkey
    - create general_monkey.gd ans .tscn
    - adjust enemy.gd tp handle enemy type
    - move enemy.tscn to /entities/enemies
    - add general_monkey idle and run sprites
    - add new constant for enemy type

---
+++ entities/enemies/general_monkey/general_monkey.gd
@@ -0,0 +1,13 @@
+extends Enemy
+
+class_name GeneralMonkey
+
+func _init():
+	initEntity()
+	meleeAttackDamage = ConstantNumber.enemyMeleeDamage
+	healthPoint = HealthPoint.new(self, ConstantNumber.enemyHealthPoint)
+	movementSpeed = ConstantNumber.enemySpeed
+	dashSpeed = 0
+	movement = MovementHandler.new(self)
+	isAttacking = false
+	enemyType = ConstantNumber.enemyMeleeType

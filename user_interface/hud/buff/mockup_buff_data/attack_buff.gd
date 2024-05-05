36e1738587a5e04fb6c4884c2cc90070bb9030b2
Xritre <98012222+Xritre@users.noreply.github.com>
2024-05-05 22:33:54 +0700

    Refactor buff and trait data
    - Refactor description of incorrect attack buff and max hp trait description

--- user_interface/hud/buff/mockup_buff_data/attack_buff.gd
+++ user_interface/hud/buff/mockup_buff_data/attack_buff.gd
@@ -5,15 +5,15 @@ class_name AttackBuff
 func _init():
 	buffName = "Attack Buff"
 	buffTexture = preload("res://icon.svg")
-	maxLevel = 2
+	maxLevel = 3
 	buffLevelData = {
 		1 : {
-			DictionaryKey.description : "Increase melee and range attack by 15%",
+			DictionaryKey.description : "Increase melee and range attack by 25%",
 			DictionaryKey.meleeAttackDamage : 0.25,
 			DictionaryKey.rangeAttackDamage : 0.25
 		},
 		2 : {
-			DictionaryKey.description : "Increase melee and range attack by 30%",
+			DictionaryKey.description : "Increase melee and range attack by 50%",
 			DictionaryKey.meleeAttackDamage : 0.5,
 			DictionaryKey.rangeAttackDamage : 0.5
 		}

4a0da1a3bbc9fb92d7f693b0b47cf254d43fa294
ArtMuchimuchi <artaoom21@gmail.com>
2024-05-17 22:17:29 +0700

    adjust_read_HP_log
    - make log reader can read and show log from different difficulty

--- utils/generate_map/log/read_HP_log.py
+++ utils/generate_map/log/read_HP_log.py
@@ -4,32 +4,62 @@ import json
 import os
 
 # List to store all player HP data
-allPlayerHP = []
-
+allPlayerHPeasy = []
+allPlayerHPnormal = []
+allPlayerHPhard = []
 # Directory where the JSON files are located
 directory = 'C:\\Users\\chisa\\Downloads\\Project\\utils\\generate_map\\log\\'
 
 # Loop to read multiple JSON files
-for i in range(1, 11):  # Adjust the range as needed
-    file_path = os.path.join(directory, f'PlayerHP{i}.json')
+for i in range(1, 4):  # Adjust the range as needed
+    file_path = os.path.join(directory, f'PlayerHPeasy{i}.json')
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
             dictPlayerHP = json.load(file)
-            allPlayerHP.append(dictPlayerHP)
+            allPlayerHPeasy.append(dictPlayerHP)
+
+for i in range(1, 4):  # Adjust the range as needed
+    file_path = os.path.join(directory, f'PlayerHPnormal{i}.json')
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            dictPlayerHP = json.load(file)
+            allPlayerHPnormal.append(dictPlayerHP)
+
+for i in range(1, 4):  # Adjust the range as needed
+    file_path = os.path.join(directory, f'PlayerHPhard{i}.json')
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            dictPlayerHP = json.load(file)
+            allPlayerHPhard.append(dictPlayerHP)
 
 # Combine all data into single x and y lists for plotting
-xAxis = []
-yHP = []
+xAxisEasy = []
+yHPEasy = []
+xAxisNormal = []
+yHPNormal = []
+xAxisHard = []
+yHPHard = []
 
-for dictPlayerHP in allPlayerHP:
-    xAxis.extend([key for key, value in dictPlayerHP.items()])
-    yHP.extend([value for key, value in dictPlayerHP.items()])
+for dictPlayerHP in allPlayerHPeasy:
+    xAxisEasy.extend([key for key, value in dictPlayerHP.items()])
+    yHPEasy.extend([value for key, value in dictPlayerHP.items()])
+
+for dictPlayerHP in allPlayerHPnormal:
+    xAxisNormal.extend([key for key, value in dictPlayerHP.items()])
+    yHPNormal.extend([value for key, value in dictPlayerHP.items()])
+
+for dictPlayerHP in allPlayerHPhard:
+    xAxisHard.extend([key for key, value in dictPlayerHP.items()])
+    yHPHard.extend([value for key, value in dictPlayerHP.items()])
     
-plt.plot(xAxis,yHP, label="Player HP Percentage")
+plt.plot(xAxisEasy,yHPEasy, label="Player HP Percentage on Easy Mode")
+plt.plot(xAxisNormal,yHPNormal, label="Player HP Percentage on Normal Mode")
+plt.plot(xAxisHard,yHPHard, label="Player HP Percentage on Hard Mode")
 
 plt.legend()
 
 plt.yticks(np.arange(0,110,step=10))
+plt.xticks(np.arange(0,110,step=10))
 
 plt.ylabel("HP Percentage")
 plt.xlabel("time")
